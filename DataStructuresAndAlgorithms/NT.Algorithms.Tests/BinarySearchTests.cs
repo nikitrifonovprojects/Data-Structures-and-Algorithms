fdaa33b5bc9436bfec742b16698262027713e7a3
nikitrifonovprojects <do_not_stop@abv.bg>
2017-05-22 14:57:11 +0300

    #13 - Implemented Implement Binary Search on a sorted array

---
+++ DataStructuresAndAlgorithms/NT.Algorithms.Tests/BinarySearchTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NT.Algorithms.Tests
+{
+    [TestClass]
+    public class BinarySearchTests
+    {
+        [TestMethod]
+        public void BinarySearchWorksCorrectly()
+        {
+            //Arrange
+            int[] array = new int[] { -50, 3, 7, -125, 4, 0, -22, -178, 99, 120, -33, 9, 5, 2, 6, 1, 8, 10, 55, 110, 12, 34, 66 };
+            Array.Sort(array);
+            int numberToFind = 8;
+            int position = 13;
+
+            var search = new BinarySearch();
+
+            //Act
+            int result = search.BinarySearchArray(array, numberToFind);
+            //Assert
+            Assert.AreEqual(position, result);
+        }
+
+        [TestMethod]
+        public void BinarySearchRecursiveWorksCorrectly()
+        {
+            //Arrange
+            int[] array = new int[] { -50, 3, 7, -125, 4, 0, -22, -178, 99, 120, -33, 9, 5, 2, 6, 1, 8, 10, 55, 110, 12, 34, 66 };
+            Array.Sort(array);
+            int numberToFind = 9;
+            int position = 14;
+
+            var search = new BinarySearch();
+
+            //Act
+            int result = search.BinarySearchRecursive(array, numberToFind);
+            //Assert
+            Assert.AreEqual(position, result);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/NT.Algorithms/BinarySearch.cs
@@ -0,0 +1,60 @@
+﻿namespace NT.Algorithms
+{
+    public class BinarySearch
+    {
+        public int BinarySearchArray(int[] array, int value)
+        {
+            int min = 0;
+            int max = array.Length - 1;
+            int middle = (min + max) / 2;
+
+            while (array[middle] != value)
+            {
+                if (min > max)
+                {
+                    return - 1;
+                }
+
+                if (array[middle] < value)
+                {
+                    min = middle++;
+                }
+                else if (array[middle] > value)
+                {
+                    max = middle--;
+                }
+            }
+
+            return middle;
+        }
+
+        public int BinarySearchRecursive(int[] array, int value)
+        {
+            return Recursive(array, value, 0, array.Length - 1);
+        }
+
+        private int Recursive(int[] array, int value, int min , int max)
+        {
+            if (min > max)
+            {
+                return - 1;
+            }
+            else
+            {
+                int middle = (min + max) / 2;
+                if (value == array[middle])
+                {
+                    return middle;
+                }
+                else if (array[middle] > value)
+                {
+                    return Recursive(array, value, min, middle - 1);
+                }
+                else
+                {
+                    return Recursive(array, value, middle + 1, max);
+                }
+            }
+        }
+    }
+}
