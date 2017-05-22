554b30280e8392c6e328ada28435b4915a7af260
nikitrifonovprojects <do_not_stop@abv.bg>
2017-05-22 16:04:59 +0300

    #14 - Implemented Implement Interpolation Search on a sorted array

---
+++ DataStructuresAndAlgorithms/NT.Algorithms.Tests/InterpolationSearchTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NT.Algorithms.Tests
+{
+    [TestClass]
+    public class InterpolationSearchTests
+    {
+        [TestMethod]
+        public void InterpolationSearchWorksCorrectly()
+        {
+            //Arrange
+            int[] array = new int[] { -50, 3, 7, -125, 4, 0, -22, -178, 99, 120, -33, 9, 5, 2, 6, 1, 8, 10, 55, 110, 12, 34, 66 };
+            Array.Sort(array);
+            int numberToFind = 10;
+            int position = 15;
+            var search = new InterpolationSearch();
+
+            //Act
+            int result = search.InterpolationArraySearch(array, numberToFind);
+
+            //Assert
+            Assert.AreEqual(position, result);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/NT.Algorithms/InterpolationSearch.cs
@@ -0,0 +1,42 @@
+﻿namespace NT.Algorithms
+{
+    public class InterpolationSearch
+    {
+        public int InterpolationArraySearch(int[] array, int value)
+        {
+            return Interpolation(array, array.Length, value);
+        }
+
+        private int Interpolation(int[] array, int length, int value)
+        {
+            int low = 0;
+            int middle = -1;
+            int high = length - 1;
+            int index = -1;
+
+            while (low <= high)
+            {
+                middle = (int)(low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low])));
+
+                if (array[middle] == value)
+                {
+                    index = middle;
+                    break;
+                }
+                else
+                {
+                    if (array[middle] < value)
+                    {
+                        low = middle + 1;
+                    }
+                    else
+                    {
+                        high = middle - 1;
+                    }
+                }
+            }
+
+            return index;
+        }
+    }
+}
