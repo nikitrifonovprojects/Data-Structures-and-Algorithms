61fa8cf7420d0d64b38d072eda6ab54a16d798f0
nikitrifonovprojects <do_not_stop@abv.bg>
2017-05-30 17:53:53 +0300

    #18 - implemented Implement Depth-First Search (DFS) on the Tree class

---
+++ DataStructuresAndAlgorithms/NT.Algorithms.Tests/DFSearchTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NT.DataStructures;
+
+namespace NT.Algorithms.Tests
+{
+    [TestClass]
+    public class DFSearchTests
+    {
+        [TestMethod]
+        public void DFSearchWorksCorrectly()
+        {
+            //Arrange
+            var tree = new Tree<int>();
+            int count = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                var node = new Tree<int>.TreeNode(count);
+                count++;
+                tree.Add(node);
+                for (int j = 0; j < 10; j++)
+                {
+                    var child = new Tree<int>.TreeNode(count);
+                    count++;
+                    tree.Add(child, node);
+                    for (int k = 0; k < 10; k++)
+                    {
+                        var newChild = new Tree<int>.TreeNode(count);
+                        count++;
+                        tree.Add(newChild, child);
+                        for (int f = 0; f < 20; f++)
+                        {
+                            var lastChild = new Tree<int>.TreeNode(count);
+                            count++;
+                            tree.Add(lastChild, newChild);
+                        }
+                    }
+                }
+            }
+
+            //Act
+            var search = new DFSearch<int>();
+            var result = search.DepthFirstSearch(tree.Root);
+            var actual = new List<int>();
+            foreach (var item in tree)
+            {
+                actual.Add(item);
+            }
+
+            //Assert
+            CollectionAssert.AreEquivalent(result, actual);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/NT.Algorithms/DFSearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using NT.DataStructures;
+
+namespace NT.Algorithms
+{
+    public class DFSearch<T> where T : IComparable<T>
+    {
+        public List<T> DepthFirstSearch(Tree<T>.TreeNode root)
+        {
+            var stack = new Stack<Tree<T>.TreeNode>();
+            var result = new List<T>();
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                var currentNode = stack.Pop();
+                result.Add(currentNode.Value);
+                foreach (var node in currentNode.NodeChildren)
+                {
+                    stack.Push(node);
+                }
+            }
+
+            return result;
+        }
+    }
+}
