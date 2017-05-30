43d0093151ff2556621f1cc9241eb7f91d7b5691
nikitrifonovprojects <do_not_stop@abv.bg>
2017-05-30 17:53:04 +0300

    #17 - implemented Implement Breadth-First Search (BFS) on the Tree class

---
+++ DataStructuresAndAlgorithms/NT.Algorithms.Tests/BFSearchTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NT.DataStructures;
+
+namespace NT.Algorithms.Tests
+{
+    [TestClass]
+    public class BFSearchTests
+    {
+        [TestMethod]
+        public void BFSearchWorksCorrectly()
+        {
+            //Arrange
+            var tree = new Tree<int>();
+            int count = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                var node = new Tree<int>.TreeNode(count);
+                count++;
+                tree.Add(node);
+                for (int j = 0; j < 10; j++)
+                {
+                    var child = new Tree<int>.TreeNode(count);
+                    count++;
+                    tree.Add(child, node);
+                    for (int k = 0; k < 10; k++)
+                    {
+                        var newChild = new Tree<int>.TreeNode(count);
+                        count++;
+                        tree.Add(newChild, child);
+                        for (int f = 0; f < 20; f++)
+                        {
+                            var lastChild = new Tree<int>.TreeNode(count);
+                            count++;
+                            tree.Add(lastChild, newChild);
+                        }
+                    }
+                }
+            }
+
+            //Act
+            var search = new BFSearch<int>();
+            var result = search.BreadthFirstSearch(tree.Root);
+            var actual = new List<int>();
+            foreach (var item in tree)
+            {
+                actual.Add(item);
+            }
+
+            //Assert
+            CollectionAssert.AreEqual(result, actual);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/NT.Algorithms/BFSearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using NT.DataStructures;
+
+namespace NT.Algorithms
+{
+    public class BFSearch<T> where T : IComparable<T>
+    {
+        public List<T> BreadthFirstSearch(Tree<T>.TreeNode root)
+        {
+            var queue = new Queue<Tree<T>.TreeNode>();
+            var result = new List<T>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                result.Add(currentNode.Value);
+                foreach (var node in currentNode.NodeChildren)
+                {
+                    queue.Enqueue(node);
+                }
+            }
+
+            return result;
+        }
+    }
+}
