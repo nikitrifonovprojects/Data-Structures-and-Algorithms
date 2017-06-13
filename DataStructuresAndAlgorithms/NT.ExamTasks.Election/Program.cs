da2311d7fdffa0728ff779df61660c982d1d0827
nikitrifonovprojects <do_not_stop@abv.bg>
2017-06-13 17:20:26 +0300

    #23 - passed tests Exam Task - Election

---
+++ DataStructuresAndAlgorithms/NT.ExamTasks.Election/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace NT.ExamTasks.Election
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int k = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            var seatsPerN = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                seatsPerN[i] = int.Parse(Console.ReadLine());
+            }
+
+            var count = Comb(seatsPerN, k);
+
+            Console.WriteLine(count);
+        }
+
+        private static BigInteger Comb(IEnumerable<int> seatsPerN, int k)
+        {
+            var combinations = new BigInteger[(100 * 1000) + 1];
+            var maxSum = 0;
+
+            combinations[0] = 1;
+
+            foreach (var seats in seatsPerN)
+            {
+                for (int i = maxSum; i >= 0; i--)
+                {
+                    if (combinations[i] > 0)
+                    {
+                        combinations[i + seats] += combinations[i];
+                        maxSum = Math.Max(maxSum, i + seats);
+                    }
+                }
+            }
+
+            var countOfSolutions = new BigInteger(0);
+            for (int f = k; f <= maxSum; f++)
+            {
+                countOfSolutions += combinations[f];
+            }
+
+            return countOfSolutions;
+        }
+    }
+}
